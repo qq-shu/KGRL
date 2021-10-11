12c4cc7b052ef692c813c4fc99f105a229b7e1a8
qq-shu <67249567+qq-shu@users.noreply.github.com>
2021-10-11 10:19:19 +0800

    Update hyperparameter_range.py

--- src/hyperparameter_range.py
+++ src/hyperparameter_range.py
@@ -1,12 +1,3 @@
-"""
- Copyright (c) 2018, salesforce.com, inc.
- All rights reserved.
- SPDX-License-Identifier: BSD-3-Clause
- For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- 
- Hyperparameter range specification.
-"""
-
 hp_range = {
     "beta": [0., .01, .02, .05, .1],
     "emb_dropout_rate": [0, .1, .2, .3],
